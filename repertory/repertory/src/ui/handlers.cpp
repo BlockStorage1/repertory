df3db38ae7e004a5dc782a6ee5dd7a9ce8cc1e68
Scott E. Graves <scott.e.graves@protonmail.com>
2025-03-27 09:58:11 -0500

    letters should be lowercase

--- repertory/repertory/src/ui/handlers.cpp
+++ repertory/repertory/src/ui/handlers.cpp
@@ -326,9 +326,9 @@ void handlers::handle_put_mount_location(const httplib::Request &req,
 void handlers::handle_get_available_locations(httplib::Response &res) {
 #if defined(_WIN32)
   constexpr const std::array<std::string_view, 26U> letters{
-      "A:", "B:", "C:", "D:", "E:", "F:", "G:", "H:", "I:",
-      "J:", "K:", "L:", "M:", "N:", "O:", "P:", "Q:", "R:",
-      "S:", "T:", "U:", "V:", "W:", "X:", "Y:", "Z:",
+      "a:", "b:", "c:", "d:", "e:", "f:", "g:", "h:", "i:",
+      "j:", "k:", "l:", "m:", "n:", "o:", "p:", "q:", "r:",
+      "s:", "t:", "u:", "v:", "w:", "x:", "y:", "z:",
   };
 
   auto available = std::accumulate(
